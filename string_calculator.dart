cb750dd8e0b445779895f5b2d5b1f73fc030c5b8
shiv.dutt <shiv.dutt@havells.com>
2025-02-11 23:38:38 +0530

    throw catch added

--- string_calculator.dart
+++ string_calculator.dart
@@ -6,10 +6,26 @@ void main() {
 
 void testSolution() {
   StringCalculator calculator = StringCalculator();
-  print("Running tests...");
+  try {
+    print("Running tests...");
 
-  print("Input: \"\"   : Output: 0 ------>  " + (calculator.add("") == 0 ? "passed" : "failed"));
-  print("Input: \"1\"   : Output: 1 ------>  " + (calculator.add("1") == 1 ? "passed" : "failed"));
-  print("Input: \"1,5\"   : Output: 6 ------>  " + (calculator.add("1,5") == 6 ? "passed" : "failed"));
-  print("Input: \"//;\n1;2\"   : Output: 3 ------>  " + (calculator.add("//;\n1;2") == 3 ? "passed" : "failed"));
+    print("Input: \"\"          : Output: 0 ------>  " + (calculator.add("") == 0 ? "passed" : "failed"));
+    print("Input: \"1\"         : Output: 1 ------>  " + (calculator.add("1") == 1 ? "passed" : "failed"));
+    print("Input: \"1,5\"       : Output: 6 ------>  " + (calculator.add("1,5") == 6 ? "passed" : "failed"));
+    print("Input: \"//;\\n1;2\" : Output: 3 ------>  " + (calculator.add("//;\n1;2") == 3 ? "passed" : "failed"));
+    print("Input: \"//;\\n1;-2,-44\"   : Output: 3 ------>  " +
+        (calculator.add("//;\n1;-2,-44") == 3 ? "passed" : "failed"));
+  } catch (e) {
+    print(e);
+  }
 }
+
+
+// output:
+
+// Running tests...
+// Input: ""          : Output: 0 ------>  passed
+// Input: "1"         : Output: 1 ------>  passed
+// Input: "1,5"       : Output: 6 ------>  passed
+// Input: "//;\n1;2" : Output: 3 ------>  passed
+// Invalid argument(s): Input: //;\n1;-2,-44 : Output: negative numbers not allowed: -2, -44
--- string_calculator_service.dart
+++ string_calculator_service.dart
@@ -3,12 +3,19 @@ class StringCalculator {
 
   int add(String numbers) {
     int sum = 0;
+    String negatives = "";
 
     List<int> numList = _extractNum.allMatches(numbers).map((match) => int.parse(match.group(0)!)).toList();
 
     for (var val in numList) {
-      assert(!val.isNegative, "negative numbers not allowed: $val");
+      if (val.isNegative) {
+        negatives = negatives + (negatives.isEmpty ? "$val" : ", $val");
+      }
       sum += val;
+    }
+
+    if (negatives.isNotEmpty) {
+      throw ArgumentError("Input: ${numbers.toString()} : Output: negative numbers not allowed: $negatives");
     }
 
     return sum;
